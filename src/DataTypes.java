a60a8604de5837f475fe2e0f42eb938e223bfec9
Raul Ortiz <Alejandro_07R@protonmail.com>
2026-03-24 21:38:57 -0500

    Day 1 : HelloWorld, DataTypes, Expressions

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class DataTypes {
+    public static void main(String[] args){
+        int myFirstNumber = 5;
+        System.out.print(myFirstNumber + "\n");
+
+        myFirstNumber = 10;
+        System.out.print(myFirstNumber + "\n");
+
+        int mySecondNumber = 10 + 5;
+        System.out.print(mySecondNumber + "\n");
+
+        int myThirdNumber = (10 + 5) + (5 * 4);
+        System.out.print(myThirdNumber + "\n");
+
+    }
 }
--- src/Expressions.java
+++ src/Expressions.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Expressions {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000;
+        System.out.println(myLastOne + "\n");
+
+        myLastOne = myLastOne - myTotal;
+
+        System.out.println(myLastOne);
+    }
 }
